c9371f7b9a24bde71e1d701bf1770b11992dd051
shakib522 <shakibhasann522@gmail.com>
2025-02-07 15:57:53 +0600

    Initial Commit

---
+++ src/main/java/com/example/selfcourier/SelfCourierApplication.java
@@ -0,0 +1,13 @@
+package com.example.selfcourier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfCourierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfCourierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/selfcourier/SelfCourierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selfcourier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfCourierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
