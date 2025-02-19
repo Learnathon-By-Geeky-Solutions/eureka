09da2cc6426f5d0bfb5876673e546a24eaa0255f
MHDabdulkader <godholiak12345@gmail.com>
2025-02-19 21:55:20 +0600

    Navbar with dropdown menus (Version 1)

---
+++ Frontend/src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    <div className=''>
+      <div>
+        <Navbar/>
+      </div>
+      <div>
+        Home 
+      </div>
+      
+    </div>
+  )
+}
+
+export default Home
